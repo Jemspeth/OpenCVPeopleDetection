3f6316c86a46d4665e9b14438017761102aa9eff
Jemspeth <spether13@gmail.com>
2017-06-14 18:31:23 -0700

    Create HOG.cpp

--- HOG.cpp
+++ HOG.cpp
@@ -1,6 +1,7 @@
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
+#include "opencv2/objdetect.hpp"
 #include "HOGCascade.hpp"
 #include <iostream>
 
@@ -9,15 +10,16 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-cv::CascadeClassifier classifier;
-cv::HOGCascadeClassifier HOGClassifier;
+CascadeClassifier classifier;
+HOGCascadeClassifier hogClassifier;
+HOGDescriptor hog;
 
 // must check for HOG first, because OpenCV3 will throw an error otherwise
-if(!HOGClassifier.load("hogcascade_pedestrians.xml"))
-  if(!classifier.load("hogcascade_pedestrians.xml"))
-    throw std::runtime_error("Could not read file");
+if(!hogClassifier.load("hogcascade_pedestrians.xml"))
+    if(!classifier.load("hogcascade_pedestrians.xml"))
+        throw std::runtime_error("Could not read file");
 
 
 
-  return 0;
+	return 0;
 }
